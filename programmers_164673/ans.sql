5b496d25de2317371d6403ed8b33bf2c06858542
min0731 <ypd04208@gmail.com>
2023-04-30 15:23:27 +0900

    Solving_04_30

---
+++ programmers_164673/ans.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/164673
+-- 게시글 작성일 기준으로 LIKE
+
+SELECT TITLE, B.BOARD_ID, REPLY_ID, R.WRITER_ID, R.CONTENTS, date_format(R.CREATED_DATE,'%Y-%m-%d') "CREATED_DATE"
+    FROM USED_GOODS_BOARD B JOIN USED_GOODS_REPLY R ON (B.BOARD_ID = R.BOARD_ID)
+    WHERE B.CREATED_DATE LIKE '2022-10%'
+    ORDER BY R.CREATED_DATE,TITLE ASC;
