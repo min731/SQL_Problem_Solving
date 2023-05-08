0f64777bcd53b2d847552f20df73956e4a31fbee
min0731 <ypd04208@gmail.com>
2023-05-08 13:57:25 +0900

    Solving_05_08

---
+++ programmers_164672/ans.sql
@@ -0,0 +1,12 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/164672
+-- CASE-WHEN-END AS 문 활용
+
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE,
+    CASE
+        WHEN STATUS = 'DONE' THEN '거래완료'
+        WHEN STATUS = 'SALE' THEN '판매중'
+        ELSE '예약중'
+    END AS 'STATUS'
+    FROM USED_GOODS_BOARD
+    WHERE DATE_FORMAT(CREATED_DATE,'%Y-%m-%d') = '2022-10-05'
+    ORDER BY BOARD_ID DESC;
