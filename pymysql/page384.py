dbe63cf722f696e231afebc9c0432200e2775c5c
min0731 <ypd04208@gmail.com>
2023-05-10 14:27:11 +0900

    Solving_05_10

--- pymysql/page384.py
+++ pymysql/page384.py
@@ -1,7 +1,7 @@
 import pymysql
 
 # 한글 사용 시, charset = 'utf8'
-conn = pymysql.connect(host='127.0.0.1',port=3306, user='root',password='',db='solodb',charset='utf8')
+conn = pymysql.connect(host='127.0.0.1',port=3306, user='root',password='1234',db='test',charset='utf8')
 
 cur = conn.cursor()
 
