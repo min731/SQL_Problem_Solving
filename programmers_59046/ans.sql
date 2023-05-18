e30713a7545bd8006801a86ec4216dcf38df6784
min0731 <ypd04208@gmail.com>
2023-05-18 18:52:08 +0900

    Solving_05_18

---
+++ programmers_59046/ans.sql
@@ -0,0 +1,11 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/59046
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+    FROM ANIMAL_INS
+    WHERE NAME IN('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+    ORDER BY ANIMAL_ID ASC;
+
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+    FROM ANIMAL_INS
+    WHERE NAME REGEXP 'Lucy|Ella|Pickle|Rogan|Sabrina|Mitty'
+    ORDER BY ANIMAL_ID ASC;
---
+++ programmers_59406/ans.sql
@@ -0,0 +1,4 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/59406
+
+SELECT COUNT(*)
+    FROM ANIMAL_INS;
