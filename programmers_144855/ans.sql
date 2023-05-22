5f14eac08f6ff323937da0310461baf0f32c03e7
min0731 <ypd04208@gmail.com>
2023-05-22 14:43:17 +0900

    Solving_05_22

---
+++ programmers_144855/ans.sql
@@ -0,0 +1,9 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/144855
+
+SELECT B.CATEGORY, SUM(BS.SALES) "TOTAL_SALES"
+    FROM BOOK B
+    INNER JOIN BOOK_SALES BS
+    ON B.BOOK_ID = BS.BOOK_ID
+    WHERE BS.SALES_DATE LIKE '2022-01%'
+    GROUP BY CATEGORY
+    ORDER BY CATEGORY ASC;
