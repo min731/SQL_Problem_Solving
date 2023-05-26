79c64a3af314c7ba2ec69579096ae091f994a8b1
min0731 <ypd04208@gmail.com>
2023-05-26 17:45:36 +0900

    Solving_05_26

---
+++ programmers_157340/ans.sql
@@ -0,0 +1,9 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/157340
+-- CASE-WHEN , IF ,SUM()
+SELECT CAR_ID, CASE
+    WHEN SUM(IF("2022-10-16" BETWEEN START_DATE AND END_DATE,1,0)) > 0 THEN "대여중"
+    ELSE "대여 가능"
+    END AS "AVAILABILITY"
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
+    GROUP BY CAR_ID
+    ORDER BY CAR_ID DESC;
